f9d70f73a4fa97e0fc48e5514d1ab6b62cb94b45
Aism793 <ismacenteno123@gmail.com>
2021-09-23 12:20:34 -0500

    Refactoring caso producto compuesto

---
+++ ProductoDomain/ProductoCompuesto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductoDomain
+{
+    public class ProductoCompuesto
+    {
+        //protected List<MovimientoCuenta> _movimientos;
+        public string Id { get; private set; }
+        public string Nombre { get; private set; }
+        public string Categoria { get; private set; }
+        public decimal Costo { get; private set; }
+        public decimal Precio { get; private set; }
+        public decimal Utilidad { get; private set; }
+        public int Cantidad { get; protected set; }
+
+        public ProductoCompuesto(string id, string nombre, string categoria, decimal costo, decimal precio)
+        {
+            Id = id;
+            Nombre = nombre;
+            Categoria = categoria;
+            Costo = costo;
+            Precio = precio;
+            Utilidad = precio - costo;
+        }
+
+        public string Vender(int cantidadProducto)
+        {
+            if (cantidadProducto < 0)
+            {
+                return "La cantidad a vender es incorrecta";
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
--- ProductoSolutionTDD/ProductoSolutionTest.cs
+++ ProductoSolutionTDD/ProductoSolutionTest.cs
@@ -95,34 +95,5 @@ namespace ProductoSolutionTDD
         }
     }
 
-    internal class ProductoCompuesto
-    {
-        //protected List<MovimientoCuenta> _movimientos;
-        public string Id { get; private set; }
-        public string Nombre { get; private set; }
-        public string Categoria { get; private set; }
-        public decimal Costo { get; private set; }
-        public decimal Precio { get; private set; }
-        public decimal Utilidad { get; private set; }
-        public int Cantidad { get; protected set; }
-
-        public ProductoCompuesto(string id, string nombre, string categoria, decimal costo, decimal precio)
-        {
-            Id = id;
-            Nombre = nombre;
-            Categoria = categoria;
-            Costo = costo;
-            Precio = precio;
-            Utilidad = precio - costo;
-        }
-
-        internal string Vender(int cantidadProducto)
-        {
-            if (cantidadProducto < 0)
-            {
-                return "La cantidad a vender es incorrecta";
-            }
-            throw new NotImplementedException();
-        }
-    }
+    
 }
